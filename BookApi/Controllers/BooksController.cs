c979e6dd8ffaef81db9e0c40114b92488a44e746
Bhargavi Lukalapu <bhargavilukalapu@gmail.com>
2026-04-29 18:59:08 -0800

    Initial Commit Book Api

---
+++ BookApi/Controllers/BooksController.cs
@@ -0,0 +1,23 @@
+﻿using BookApi.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace BookApi.Controllers
+{
+    // https://localhost:7082/api/books
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class BooksController: ControllerBase
+    {
+        private Book[] _books = new Book[]
+        {
+            new Book { Id=1, Author="Author one", Title="Book one"},
+            new Book { Id=2, Author="Author Two", Title="Book Two"},
+            new Book { Id=3, Author="Author Three", Title="Book Three"}
+        };
+
+        [HttpGet]    //endpoint http get
+        public ActionResult<IEnumerable<Book>> GetBooks()
+        {
+            return Ok(_books);
+        }
+    }
+}
