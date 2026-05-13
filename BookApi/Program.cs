733a7363968f9f1ea80cb3f24f69b64aec6804f5
Bhargavi Lukalapu <bhargavilukalapu@gmail.com>
2026-05-12 22:30:00 -0800

    Added CORS confugration and API routing

--- BookApi/Program.cs
+++ BookApi/Program.cs
@@ -8,12 +8,21 @@ namespace BookApi
 
             //add services
             builder.Services.AddControllers();
-
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("MyCors", builder =>
+                {
+                    builder.WithOrigins("http://localhost:4200")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+                });
+            });
             var app = builder.Build();
 
             //add mapping
+            app.UseCors("MyCors");
             app.MapControllers();
-
+           
             app.MapGet("/", () =>
             {
                 return Results.Redirect("api/books");
